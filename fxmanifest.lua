397a2df4b9c46f9c01b02f283f21ddb681925b83
Ginka <116312031+GinkaRP@users.noreply.github.com>
2023-09-15 15:03:54 +0100

    fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,13 +2,9 @@ fx_version 'cerulean'
 lua54 'yes'
 game 'gta5'
 
-Author 'Ginka'
+Author 'Ginka#3318'
 
 client_scripts {
 	'LUA/config/*.lua',
 	'LUA/client/*.lua',
 }
-
---[[server_scripts {
-	'server.lua',
-}]]
